7a2c8ffdb2148b96742bc3a85a24c954b302cc91
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-09-05 11:47:51 -0700

    fixed minor syntax error

--- end.js
+++ end.js
@@ -1,7 +1,7 @@
 const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = document.querySelector('#mostRecentScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
@@ -31,4 +31,4 @@ saveHighScore = e => {
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
     window.location.assign('/')
-}.
+}
